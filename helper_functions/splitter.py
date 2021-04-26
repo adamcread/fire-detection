80b22f05868e36c089559bf4829b46922bca6d11
Adam Read <fsfr78@ncc1.clients.dur.ac.uk>
2021-04-26 11:50:29 +0100

    whatever

--- helper_functions/splitter.py
+++ helper_functions/splitter.py
@@ -1,16 +1,9 @@
 import os
 
-root = "./dataset/original_datasets/kim-lee-2019/pos"
-dest = "./dataset/split_dataset/kim-lee-2019/pos"
+root = "./dataset/test_dataset/resized"
+dest = "./dataset/test_dataset/split"
 
 for vid in os.listdir(root):
     name, extension = vid.split(".")
 
-    os.system(
-        "ffmpeg -i '{root}/{vid}' -map 0 -codec copy -reset_timestamps 1 -f segment -segment_time 00:10 '{dest}/{name}-%03d.mp4'".format(
-                                                                                                                                            root=root, 
-                                                                                                                                            vid=vid,
-                                                                                                                                            name=name,
-                                                                                                                                            dest=dest
-                                                                                                                                        )
-    )
+    os.system("ffmpeg -i {root}/{vid} -c:v libx264 -crf 22 -map 0 -segment_time 10 -reset_timestamps 1 -g 300 -sc_threshold 0 -force_key_frames 'expr:gte(t,n_forced*10)' -f segment {dest}/{name}-%03d.mp4".format(root=root,vid=vid,name=name,dest=dest))
